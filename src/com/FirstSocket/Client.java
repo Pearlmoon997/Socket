7e7b2fe8b27e20f9535f8fa845e48c0bc0decfee
moongi <pearlmoon997@gmail.com>
2022-10-01 17:30:35 +0900

    소켓 First

--- src/com/FirstSocket/Client.java
+++ src/com/FirstSocket/Client.java
@@ -6,15 +6,15 @@ import java.net.Socket;
 
 public class Client {
     public static void main(String[] args) {
-        Socket socket = null;
-        BufferedReader br = null;
-        BufferedReader br2 = null;
-        PrintWriter pw = null;
+        Socket socket = null;       //Server 와 통신하기 위한 소켓
+        BufferedReader br = null;       //Server 로 부터 데이터를 읽어들이기 위한 입력 스트림
+        BufferedReader br2 = null;      //키보드로부터 읽어들이기 위한 입력 스트림
+        PrintWriter pw = null;      //Server 로 내보내기 위한 출력 스트림
         InetAddress ia = null;
 
         try {
-            ia = InetAddress.getLocalHost();
-            socket = new Socket(ia, 1234);
+            ia = InetAddress.getLocalHost();        //Server 로 접속하기 위한 로컬호스트 주소 가져옴
+            socket = new Socket(ia, 1234);      //서버의 포트 번호
             br = new BufferedReader(new InputStreamReader(socket.getInputStream()));
             br2 = new BufferedReader(new InputStreamReader(System.in));
 
@@ -26,11 +26,11 @@ public class Client {
 
         } try {
             System.out.print("서버로 보낼 메세지 : ");
-            String data = br2.readLine();
-            pw.println(data);
+            String data = br2.readLine();       //키보드로부터 입력
+            pw.println(data);       //서버로 데이터 전송
             pw.flush();
 
-            String str2 = br.readLine();
+            String str2 = br.readLine();        //서버로부터 되돌아오는 데이터 읽어옴
             System.out.print("서버의 응답 : " + str2);
             socket.close();
         } catch (IOException e) {
--- src/com/FirstSocket/Server.java
+++ src/com/FirstSocket/Server.java
@@ -6,10 +6,10 @@ import java.net.Socket;
 
 public class Server {
     public static void main(String[] args) {
-        Socket socket = null;
-        ServerSocket serverSocket = null;
-        BufferedReader br = null;
-        PrintWriter pw = null;
+        Socket socket = null;       //Client 와 통신하기 위한 소켓
+        ServerSocket serverSocket = null;       //서버 생성을 위한 서버소켓
+        BufferedReader br = null;       //Client 로 부터 데이터를 읽어오기 위한 입력 스트림
+        PrintWriter pw = null;      //Client 로 데이터를 보내기 위한 출력 스트림
 
         try {
             serverSocket = new ServerSocket(1234);
@@ -17,17 +17,20 @@ public class Server {
             System.out.println("해당 포트가 열려있습니다,");
         } try {
             System.out.println("서버 오픈");
+            //서버를 생성, Client 접속 대기
             socket = serverSocket.accept();
 
+            //입력 스트림 생성
             br = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+            //출력 스트림 생성
             pw = new PrintWriter(new BufferedWriter(new OutputStreamWriter(socket.getOutputStream())));
 
             String str = null;
-            str = br.readLine();
+            str = br.readLine();        //Client 로 부터 데이터 읽어옴
             System.out.println("Client의 메세지 : " + str);
             pw.write(str);
-            pw.flush();
-            socket.close();
+            pw.flush();     //버퍼링으로 인해 기록되지 않은 데이터를 출력스트림에 모두 출력
+            socket.close();     //출력되지 않은 스트림 모두 출력하고 소켓 닫음
         } catch (IOException e) {
 
         }
